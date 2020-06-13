b2e714a2ce9c5a88c891c0e766678318cd30457d
Siddhant Misra <msiddhant22@gmail.com>
2020-06-12 19:23:52 -0700

    added sets

---
+++ Collections/Sets/5th_one_frozenset.py
@@ -0,0 +1,9 @@
+binary_elements = frozenset({'0', '1'})
+
+# binary_elements.add('2')  # AttributeError: 'frozenset' object has no attribute 'add'
+
+students = {'ram', 'rahim', 'robert'}
+students.add('ramanaji')
+
+print(f'{binary_elements = }')
+print(f'{students        = }')
